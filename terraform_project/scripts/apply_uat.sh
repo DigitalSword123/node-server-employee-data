c44637b47939227365efacb4971f9e89f2449dff
Amiya Rana <ranaamiya70@gmail.com>
2022-06-12 21:44:54 +0530

    '' added

--- terraform_project/scripts/apply_uat.sh
+++ terraform_project/scripts/apply_uat.sh
@@ -14,6 +14,6 @@ cd terraform_project
 ls -al
 terraform init -backend-config key="employe-node-server/${TARGET_ENV_UAT}/terraform.tfstate"
 
-terraform plan -var-file=$VAR_FILE 
+terraform plan -var-file='$VAR_FILE' 
 
-terraform apply -var-file=$VAR_FILE  -auto-approve
+terraform apply -var-file='$VAR_FILE'  -auto-approve
