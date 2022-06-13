6a92cf7a4ddeecafefd44b2fd587bb673933351f
Amiya Rana <ranaamiya70@gmail.com>
2022-06-13 18:39:44 +0530

    modified 10

--- terraform_project/scripts/apply_dev.sh
+++ terraform_project/scripts/apply_dev.sh
@@ -22,7 +22,9 @@ cd terraform_project
 
 ls -al
 
-terraform init -backend-config="key=employe-node-server/$env/terraform.tfstate"
+terraform init 
+
+# -backend-config="key=employe-node-server/$env/terraform.tfstate"
 
 
 terraform plan -var-file=dev-ap-south-1.tfvars
