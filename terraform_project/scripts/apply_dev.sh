adb02f750384cba9bf1a904f6ce384a66735ff86
Amiya Rana <ranaamiya70@gmail.com>
2022-06-13 17:46:05 +0530

    modified 5

--- terraform_project/scripts/apply_dev.sh
+++ terraform_project/scripts/apply_dev.sh
@@ -22,8 +22,8 @@ ls -al
 
 terraform init -backend=true \
   -backend-config key=employe-node-server/${TARGET_ENV_DEV}/terraform.tfstate \
-  -backend-config "access_key = AKIAZ332BW4JK4JM2BMC" \
-  -backend-config "secret_key = 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"
+  -backend-config "access_key=AKIAZ332BW4JK4JM2BMC" \
+  -backend-config "secret_key=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"
 
 
 terraform plan -var-file="dev-ap-south-1.tfvars"
--- terraform_project/scripts/apply_qa.sh
+++ terraform_project/scripts/apply_qa.sh
@@ -19,8 +19,8 @@ ls -al
 
 terraform init -backend=true \
   -backend-config key=employe-node-server/${TARGET_ENV_QA}/terraform.tfstate \
-  -backend-config "access_key = AKIAZ332BW4JK4JM2BMC" \
-  -backend-config "secret_key = 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"  
+  -backend-config "access_key=AKIAZ332BW4JK4JM2BMC" \
+  -backend-config "secret_key=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"  
 
 terraform plan -var-file="$VAR_FILE"
 
