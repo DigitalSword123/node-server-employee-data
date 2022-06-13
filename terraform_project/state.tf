251c129dcfbe25acdf5f5cc4e473ef6f62fa52c1
Amiya Rana <ranaamiya70@gmail.com>
2022-06-13 21:27:02 +0530

    added 12

--- terraform_project/state.tf
+++ terraform_project/state.tf
@@ -8,8 +8,8 @@ terraform {
     # key     = "employe-node-server/env/terraform.tfstate" # this key 
     # is being called in shell commands present in scripts folder
     region     = "ap-south-1"
-    AWS_ACCESS_KEY_ID  = "AKIAZ332BW4JK4JM2BMC"
-    AWS_SECRET_ACCESS_KEY  = "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"
+    access_key = "AKIAZ332BW4JK4JM2BMC"
+    secret_key = "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"
     # access_key = "AKIAZ332BW4JOYRQTLGD"
     # secret_key = "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"
   }
