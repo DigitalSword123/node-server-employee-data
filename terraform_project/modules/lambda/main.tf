3c2c41d495cd9a0161fe90371de49bd299266e6f
Amiya Rana <ranaamiya70@gmail.com>
2022-06-15 17:11:32 +0530

    id principal modified 5

--- terraform_project/modules/lambda/main.tf
+++ terraform_project/modules/lambda/main.tf
@@ -91,6 +91,6 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role = var.role
+  role = "Lambda-nodejs-executionrole"
   policy_arn = "${aws_iam_policy.lambda_logging_employee.arn}"
 }
