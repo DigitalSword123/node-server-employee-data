a677f6fc5110079a0350f12fa9454b3f6a0a721f
Amiya Rana <ranaamiya70@gmail.com>
2022-07-29 18:39:49 +0530

    redeploy

--- terraform_project/state.tf
+++ terraform_project/state.tf
@@ -1,12 +1,8 @@
 # tis .tfstate file in s3 bucket will store already provisioned
 # infrastrucrures like lambda, bucket which are created previous terraform
-# apply command
-# root access key
-# access_key = "AKIAZ332BW4JOYRQTLGD" 
-# secret_key = "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"
 terraform {
   backend "s3" {
-    bucket     = "employee-data-node-terraform-state-bucket"
+    bucket     = "node-terraform-state-bucket"
     encrypt    = false
     region     = "ap-south-1"
   }
