56271f3c8118af4d089608e501c2aa4dd243693d
Amiya Rana <ranaamiya70@gmail.com>
2022-07-29 23:10:04 +0530

    test 8

--- terraform_project/scripts/apply_dev.sh
+++ terraform_project/scripts/apply_dev.sh
@@ -22,6 +22,6 @@ terraform init \
 
 
 
-terraform plan -var-file=${VAR_FILE} -out=tfplan
+terraform plan -var-file=${VAR_FILE} -out=plan.tfplan
 
-terraform apply "tfplan"
+terraform apply "plan.tfplan"
