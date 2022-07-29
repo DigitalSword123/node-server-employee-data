fd1318e1112c6111490e0a17e4cff43f04d645d5
Amiya Rana <ranaamiya70@gmail.com>
2022-07-29 23:02:58 +0530

    test 6

--- terraform_project/scripts/apply_dev.sh
+++ terraform_project/scripts/apply_dev.sh
@@ -14,7 +14,6 @@ VAR_FILE=`cat vars/${TARGET_ENV_DEV}-ap-south-1.tfvars`
 
 echo "${VAR_FILE}"
 
-terraform fmt
 
 terraform init \
  -backend-config="key=employe-node-server/${TARGET_ENV_DEV}/terraform.tfstate" \
