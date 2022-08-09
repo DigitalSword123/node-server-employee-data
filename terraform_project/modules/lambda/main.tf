bbb6d91a91d93606f11942e8003d7c8b1ff68924
Amiya Rana <ranaamiya70@gmail.com>
2022-08-09 20:59:25 +0530

    final database test 2

--- terraform_project/modules/lambda/main.tf
+++ terraform_project/modules/lambda/main.tf
@@ -83,7 +83,7 @@ resource "aws_lambda_permission" "allow_invoke_permission" {
   # for_each      = var.principal_ids
   principal     = "877760304415"
 }
-
+ 
 
 # This is to optionally manage the CloudWatch Log Group for the Lambda Function.
 # If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
