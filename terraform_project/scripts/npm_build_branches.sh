320aea0d0223ce05907b39428b011aeefce563f4
Amiya Rana <ranaamiya70@gmail.com>
2022-09-05 22:45:51 +0530

    last change in main

--- terraform_project/scripts/npm_build_branches.sh
+++ terraform_project/scripts/npm_build_branches.sh
@@ -7,14 +7,17 @@ node --version
 npm install --save-dev
 
 # https://www.jfrog.com/confluence/display/JFROG/QuickStart+Guide%3A+JFrog+Self-Hosted
-# http://localhost:8082/artifactory
+# http://localhost:8081/artifactory
 # used jfrog 7.5.5 and java version "11.0.12" 2021-07-20 LTS
+# https://www.youtube.com/watch?v=F-Tb0OFaaKQ
 
 # artifactory base url
 # go to install folder and then app->bin->artifactory.bat
 # click on localhost http://localhost:8082/artifactory
 # https://amiya.devops.com/
 # https://amiya.devops.com/artifactory/npm/
+
+# https://devopsamiya.jfrog.io/ui/admin/repositories/local/
 
 
 echo "=================build Snapshot Artifacts BEGIN====================="
