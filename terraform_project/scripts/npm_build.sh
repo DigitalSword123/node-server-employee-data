fe4ad88ea74f4d06ac2af9a443530218cfc283d9
Amiya Rana <ranaamiya70@gmail.com>
2022-09-06 18:27:53 +0530

    new code 5

--- terraform_project/scripts/npm_build.sh
+++ terraform_project/scripts/npm_build.sh
@@ -20,34 +20,44 @@ npm install --save-dev
 # https://devopsamiya.jfrog.io/ui/admin/repositories/local/
 
 
-if[$CIRCLE_BRANCH=="main"] 
-then
-    mkdir ~/.ssh && ls -alrt ~/.ssh
-    cat ~/ssh_keys/id_rsa >> ls -alrt ~/.ssh/id_rsa
-    cat ~/ssh_keys/known_hosts >> ls -alrt ~/.ssh/known_hosts
-    chmod 400 ~/.ssh/id_rsa && chmod 400 ~/.ssh/known_hosts
-    ls -alrt ~/.ssh
-    git remote set-url origin $COMPUTED_SSH_URL
-    git checkout main
-    echo "=================build release Artifacts BEGIN====================="
-    npm run release
-    echo "=================build release Artifacts END====================="
+# if[$CIRCLE_BRANCH=="main"] 
+# then
+#     mkdir ~/.ssh && ls -alrt ~/.ssh
+#     cat ~/ssh_keys/id_rsa >> ls -alrt ~/.ssh/id_rsa
+#     cat ~/ssh_keys/known_hosts >> ls -alrt ~/.ssh/known_hosts
+#     chmod 400 ~/.ssh/id_rsa && chmod 400 ~/.ssh/known_hosts
+#     ls -alrt ~/.ssh
+#     git remote set-url origin $COMPUTED_SSH_URL
+#     git checkout main
+#     echo "=================build release Artifacts BEGIN====================="
+#     npm run release
+#     echo "=================build release Artifacts END====================="
 
-    echo "=================publish release to Artifactory BEGIN====================="
-    npm publish --registry $ARTIFACTORY_LOC
-    echo "=================publish release to Artifactory END====================="
+#     echo "=================publish release to Artifactory BEGIN====================="
+#     npm publish --registry $ARTIFACTORY_LOC
+#     echo "=================publish release to Artifactory END====================="
 
-    echo "=================post release - updating the next snapshot version in package.json====================="
-    rm -f package-lock.json
-    npm version $NEXT_DEVELOPEMENT_VERSION -m "nest developement version $NEXT_DEVELOPEMENT_VERSION updated in package.json"
-    git push --set-upstream origin main
-else 
-    echo "=================build snapshot Artifacts BEGIN====================="
-    echo "building Zip files for deployement"
-    npm run build
-    echo "=================build snapshot Artifacts END====================="
+#     echo "=================post release - updating the next snapshot version in package.json====================="
+#     rm -f package-lock.json
+#     npm version $NEXT_DEVELOPEMENT_VERSION -m "nest developement version $NEXT_DEVELOPEMENT_VERSION updated in package.json"
+#     git push --set-upstream origin main
+# else 
+#     echo "=================build snapshot Artifacts BEGIN====================="
+#     echo "building Zip files for deployement"
+#     npm run build
+#     echo "=================build snapshot Artifacts END====================="
 
-    echo "=================publish snapshot to Artifactory BEGIN====================="
-    npm publish --registry $ARTIFACTORY_URL
-    echo "=================publish snapshot to Artifactory END====================="
-fi
+#     echo "=================publish snapshot to Artifactory BEGIN====================="
+#     npm publish --registry $ARTIFACTORY_URL
+#     echo "=================publish snapshot to Artifactory END====================="
+# fi
+
+
+echo "=================build snapshot Artifacts BEGIN====================="
+echo "building Zip files for deployement"
+npm run build
+echo "=================build snapshot Artifacts END====================="
+
+echo "=================publish snapshot to Artifactory BEGIN====================="
+npm publish --registry $ARTIFACTORY_URL
+echo "=================publish snapshot to Artifactory END====================="
