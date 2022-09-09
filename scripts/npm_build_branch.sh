b9a6a399b7670903379a7ce940df23b2b7a05707
Amiya Rana <ranaamiya70@gmail.com>
2022-09-09 15:37:02 +0530

    npm_build_branch.sh changed

--- scripts/npm_build_branch.sh
+++ scripts/npm_build_branch.sh
@@ -25,43 +25,8 @@ npm install --save-dev
 # artifactory base url
 # go to install folder and then app->bin->artifactory.bat
 # click on localhost http://localhost:8082/artifactory
-# https://amiya.devops.com/
-# https://amiya.devops.com/artifactory/npm/
 
 # https://devopsamiya.jfrog.io/ui/admin/repositories/local/
-
-
-# if[$CIRCLE_BRANCH=="main"] 
-# then
-#     mkdir ~/.ssh && ls -alrt ~/.ssh
-#     cat ~/ssh_keys/id_rsa >> ls -alrt ~/.ssh/id_rsa
-#     cat ~/ssh_keys/known_hosts >> ls -alrt ~/.ssh/known_hosts
-#     chmod 400 ~/.ssh/id_rsa && chmod 400 ~/.ssh/known_hosts
-#     ls -alrt ~/.ssh
-#     git remote set-url origin $COMPUTED_SSH_URL
-#     git checkout main
-#     echo "=================build release Artifacts BEGIN====================="
-#     npm run release
-#     echo "=================build release Artifacts END====================="
-
-#     echo "=================publish release to Artifactory BEGIN====================="
-#     npm publish --registry $ARTIFACTORY_LOC
-#     echo "=================publish release to Artifactory END====================="
-
-#     echo "=================post release - updating the next snapshot version in package.json====================="
-#     rm -f package-lock.json
-#     npm version $NEXT_DEVELOPEMENT_VERSION -m "nest developement version $NEXT_DEVELOPEMENT_VERSION updated in package.json"
-#     git push --set-upstream origin main
-# else 
-#     echo "=================build snapshot Artifacts BEGIN====================="
-#     echo "building Zip files for deployement"
-#     npm run build
-#     echo "=================build snapshot Artifacts END====================="
-
-#     echo "=================publish snapshot to Artifactory BEGIN====================="
-#     npm publish --registry $ARTIFACTORY_URL
-#     echo "=================publish snapshot to Artifactory END====================="
-# fi
 
 echo "=================build snapshot Artifacts BEGIN====================="
 echo "building Zip files for deployement"
@@ -77,15 +42,7 @@ echo "=================publish snapshot to Artifactory END====================="
 
 ls -al
 
-cd dist-${MODULE_NAME}
+cd target
 
+echo "------------printing target folder files ------------------"
 ls -al 
-
-echo "*************printing modules of terrafrom  start****************"
-cd modules
-
-cd lambda
-
-ls -al
-
-echo "*************printing modules of terrafrom  end****************"
