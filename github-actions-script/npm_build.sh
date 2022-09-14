3b6694dd0d0434805d21c4194ff20bc350430a19
Amiya Rana <ranaamiya70@gmail.com>
2022-09-14 20:10:15 +0530

    test

--- github-actions-script/npm_build.sh
+++ github-actions-script/npm_build.sh
@@ -38,8 +38,8 @@ then
     sudo mkdir /.ssh && ls -alrt /.ssh
     # sudo cat ./ssh_keys/id_ed25519 >> ls -alrt /.ssh/id_ed25519
     # sudo cat ./ssh_keys/known_hosts >> ls -alrt /.ssh/known_hosts
-    sudo cat ./ssh_keys/id_ed25519 /.ssh/id_ed25519
-    sudo cat ./ssh_keys/known_hosts /.ssh/known_hosts
+    sudo cp ./ssh_keys/id_ed25519 /.ssh/id_ed25519
+    sudo cp ./ssh_keys/known_hosts /.ssh/known_hosts
     sudo chmod 400 /.ssh/id_rsa && chmod 400 /.ssh/known_hosts
     eval "$(ssh-agent -s)"
     ssh-add /.ssh/id_ed25519
